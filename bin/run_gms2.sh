b0a18b0d273d06845fb75817b41be6e7d7253795
apfennig3 <apfennig3@gatech.edu>
2020-02-24 12:05:37 -0500

    Updated paths

--- bin/run_gms2.sh
+++ bin/run_gms2.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 genome=$1
+file_name=$(echo $genome | sed -e 's/\/home\/projects\/group-a\/Team1-GenomeAssembly\/assembled_output\///') 
 # create tmp folder where to save output 
-tmp_dir=$(mktemp -d -t gms2XXXXXXXXXX --tmpdir=/home/apfennig3/Team1-GenePrediction/)
-echo $tmp_dir
-./gms2.pl --seq $genome --genome-type bacteria --output $tmp_dir/output.gtf
+#tmp_dir=$(mktemp -d -t gms2XXXXXXXXXX --tmpdir=/home/projects/group-a/Team1-GenePrediction/)
+#echo $tmp_dir
+#echo $file_name
+mkdir -p /home/projects/group-a/Team1-GenePrediction/gms2_results/
+/home/projects/group-a/bin/gms2.pl --seq $genome --genome-type bacteria --output /home/projects/group-a/Team1-GenePrediction/gms2_results/output.gff --species E.coli --format gff --fnn /home/projects/group-a/Team1-GenePrediction/gms2_results/$file_name
