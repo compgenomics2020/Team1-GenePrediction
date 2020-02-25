6872922db703b8c677f884cbed3366dde76c5ae8
apfennig3 <apfennig3@gatech.edu>
2020-02-25 09:01:18 -0500

    Script to run diamond. Takes path_to_samples in fasta format and path_to_output as input. Will iterate over all files with the extension .fasta in path_to_samples and save it in path_to_output

---
+++ bin/run_diamond.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+sample_path=$1
+path_output=$2
+path_to_database='/home/projects/group-a/Team1-GenePrediction/db/nr_enterobacteriaceae.dmnd'
+if [[ ! -d $path_output ]]
+then
+   mkdir -p $path_output
+fi
+
+for file in $(ls ${sample_path}*.fasta)
+do
+    output_file=$( echo $file | sed -e "s|$sample_path|$path_output|g" -e "s|fasta|tbl|g" )
+    /home/projects/group-a/bin/Diamond blastx --query ${file} --db ${path_to_database} --out $output_file --outfmt 6 --threads 12 --max-target-seqs 1
+done
