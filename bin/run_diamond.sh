53dfb099f3f622168da1705aef7964f3c5757a5e
apfennig3 <apfennig3@gatech.edu>
2020-03-02 14:01:41 -0500

    Added output columns

--- bin/run_diamond.sh
+++ bin/run_diamond.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sample_path=$1
 path_output=$2
-path_to_database='/home/projects/group-a/Team1-GenePrediction/db/nr_enterobacteriaceae.dmnd'
+path_to_database='/home/projects/group-a/Team1-GenePrediction/db/swissprot.dmnd'
 if [[ ! -d $path_output ]]
 then
    mkdir -p $path_output
@@ -10,5 +10,5 @@ fi
 for file in $(ls ${sample_path}*.fasta)
 do
     output_file=$( echo $file | sed -e "s|$sample_path|$path_output|g" -e "s|fasta|tbl|g" )
-    /home/projects/group-a/bin/Diamond blastx --query ${file} --db ${path_to_database} --out $output_file --outfmt 6 --threads 12 --max-target-seqs 1
+    /home/projects/group-a/bin/Diamond blastx --query ${file} --db ${path_to_database} --out $output_file --outfmt 6 sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore slen qlen qcovhsp --threads 12 --max-target-seqs 1
 done
