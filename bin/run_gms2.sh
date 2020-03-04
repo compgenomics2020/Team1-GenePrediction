5aafc0773ec36a26408b5e666c22a845d0ac5946
apfennig3 <apfennig3@gatech.edu>
2020-03-04 10:17:48 -0500

    Added if statement to check if output directory exits

--- bin/run_gms2.sh
+++ bin/run_gms2.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 genome=$1
-file_name=$(echo $genome | sed -e 's/\/home\/projects\/group-a\/Team1-GenomeAssembly\/assembled_output\///') 
+file_name=$(echo $genome | sed -e 's|.*\/||g' -e 's/.fasta//') 
 # create tmp folder where to save output 
 #tmp_dir=$(mktemp -d -t gms2XXXXXXXXXX --tmpdir=/home/projects/group-a/Team1-GenePrediction/)
 #echo $tmp_dir
-#echo $file_name
-mkdir -p /home/projects/group-a/Team1-GenePrediction/gms2_results/
-/home/projects/group-a/bin/gms2.pl --seq $genome --genome-type bacteria --output /home/projects/group-a/Team1-GenePrediction/gms2_results/output.gff --species E.coli --format gff --fnn /home/projects/group-a/Team1-GenePrediction/gms2_results/$file_name
+echo $file_name
+if [ ! -d /home/projects/group-a/Team1-GenePrediction/results/gms2_results/ ]
+then
+    mkdir -p /home/projects/group-a/Team1-GenePrediction/results/gms2_results/
+fi
+/home/projects/group-a/bin/gms2.pl --seq $genome --genome-type bacteria --output /home/projects/group-a/Team1-GenePrediction/gms2_results/${file_name}.gff --species E.coli --format gff --fnn /home/projects/group-a/Team1-GenePrediction/gms2_results/${file_name}.fasta
