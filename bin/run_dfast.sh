f00a8764a785e84e1526383d4925ffabcfb5dbbe
apfennig3 <apfennig3@gatech.edu>
2020-03-04 10:19:05 -0500

    DFAST and script to run DFAST. Runs only structural annotation part of DFAST

--- bin/run_dfast.sh
+++ bin/run_dfast.sh
@@ -1,11 +1,67 @@
 #!/usr/bin/bash
+#activate environment
 source activate geneprediction
-genome=$1
-output=$2
-genome_name=$( echo $genome | sed -e "s|..\/..\/Team1-GenomeAssembly\/assembled_output\/||g" -e "s|.fasta||g" )
-../../dfast_core/dfast -g $genome -o $output --organism Ecoli --minimum_length 120 --use_prodigal --gcode 11 --cpu 8 --force  #--use_rnammer bact
-mv ${output}genome.gff ${output}${genome_name}.gff
-mv ${output}protein.faa ${output}${genome_name}_protein.faa
-mv ${output}cds.fna ${output}${genome_name}_cds.fna
-mv ${output}rna.fna ${output}${genome_name}_rna.fna
-rm ${output}application.log
+v=0
+g=0
+r=0
+while getopts ":hi:o:grv" opt
+do
+    case $opt in
+       h )
+         echo " -i <input of file> -o <path to output> -g <run GMS2 instead of prodigal> -r <run RNAmmer instead of barrnarp> -v <verbose> "
+         ;;
+       i )
+         i=$OPTARG
+         ;;
+       o )
+         o=$OPTARG
+         ;;
+       r )
+         r=1
+         ;;
+       g )
+         g=1
+         ;;
+       v )
+         v=1
+         ;;
+     esac
+done
+genome=$i
+output=$o
+#get file name
+genome_name=$( echo $genome | sed -e "s|.*\/||g" -e "s|.fasta||g" )
+
+# create output directory if doesn't exist
+if [ ! -d $output ]
+then
+    mkdir -p ${output}
+fi
+
+if [ "$v" -eq 1 ]
+then
+    echo Analyzing $genome_name
+fi
+
+# set CDS prediction tool
+if [ "$g" -eq 1 ]
+then
+    cds_tool='--use_genemarks2 bact'
+else
+    cds_tool='--use_prodigal'
+fi
+
+# run dfast with or without RNAmmer
+if [ "$r" -eq 1 ]
+then
+    ./dfast_core/dfast -g $genome -o tmp_dfast --organism Ecoli --minimum_length 120 $cds_tool --gcode 11 --cpu 8 --use_rnammer bact
+else
+    ./dfast_core/dfast -g $genome -o tmp_dfast --organism Ecoli --minimum_length 120 $cds_tool --gcode 11 --cpu 8
+fi
+
+# move files in final path and clean up
+mv tmp_dfast/genome.gff ${output}${genome_name}.gff
+mv tmp_dfast/protein.faa ${output}${genome_name}_protein.faa
+mv tmp_dfast/cds.fna ${output}${genome_name}_cds.fna
+mv tmp_dfast/rna.fna ${output}${genome_name}_rna.fna
+rm -r tmp_dfast
