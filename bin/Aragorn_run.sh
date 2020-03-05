a9397b48d7e01379fc9d44ab914b00f8a8e6162c
Maria Ahmad <mahmad42@gatech.edu>
2020-03-04 21:30:11 -0500

    Aragorn script

--- bin/Aragorn_run.sh
+++ bin/Aragorn_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd /home/projects/group-a/Team1-GenomeAssembly/assembled_output/$i;
-for i in *.fasta; do /home/projects/group-a/Tools/aragorn1.2.38/./aragorn  /home/projects/group-a/Team1-GenomeAssembly/assembled_output/$i -o /home/projects/group-a/Team1-GenePrediction/results/Aragornresults/$i;
-cd home/projects/group-a/gene_predictions/Aragornresults/; rename -v _trim_assembled.fasta _Aragorn_predicted *_trim_assembled.fasta; done                   
+for i in *.fasta; do /home/projects/group-a/Tools/aragorn1.2.38/./aragorn  /home/projects/group-a/Team1-GenomeAssembly/assembled_output/$i -seq -w -o /home/projects/group-a/Team1-GenePrediction/results/Aragornresults/$i;
+cd /home/projects/group-a/Team1-GenePrediction/Aragornresults/; rename -v _trim_assembled.fasta _Aragorn_predicted *_trim_assembled.fasta; done                   
